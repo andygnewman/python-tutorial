5d7e25c35763871bbff970c9ecc2c48e5b7f2968
Andy <andygnewman@gmail.com>
2015-04-01 11:28:22 +0100

    exercises 7 to 12

---
+++ ex10a.py
@@ -0,0 +1,5 @@
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i,
+
+#this creates a little spinner in the command line
---
+++ ex8.py
@@ -0,0 +1,13 @@
+formatter = "%r %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight.")
+#in last line of code it prints in single quotes all except the line with
+#a ' in it, which it puts in double quotes
