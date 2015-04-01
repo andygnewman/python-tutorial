d6d5eddf6cbf262eb0bbecb966c22b218ac78b86
Andy <andygnewman@gmail.com>
2015-04-01 16:01:08 +0100

    exercises 21 to 26

---
+++ ex21.py
@@ -0,0 +1,30 @@
+def add(a, b):
+    print "ADDING %d + %d" % (a, b)
+    return a + b
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+def divide(a, b):
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
+
+print "Let's do some math with just functions!"
+
+age = add(30, 12)
+height = subtract(200, 28)
+weight = multiply(20, 4)
+iq = divide(300, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
